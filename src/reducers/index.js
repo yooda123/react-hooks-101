38ec4a6754907c83236e075fe8529469851315bd
yooda123 <yosukeee.o@gmail.com>
2020-12-19 15:03:36 +0900

    create reducer for events.

---
+++ src/reducers/index.js
@@ -0,0 +1,49 @@
+// action = {
+//   type: 'CREATE_EVENT',
+//   title: '2020東京オリンピックのお知らせ',
+//   body: '2020年に東京でオリンピックを開催します。つきましては・・・'
+// }
+
+// #before
+// state = []
+// 
+// #after
+// state = [
+//     {id: 1, title: 'タイトル1', body: 'ボディー1'},
+//     {id: 2, title: 'タイトル2', body: 'ボディー2'},
+//     {id: 3, title: 'タイトル3', body: 'ボディー3'},
+// ]
+
+// #before
+// state = [
+//     {id: 1, title: 'タイトル1', body: 'ボディー1'},
+//     {id: 2, title: 'タイトル2', body: 'ボディー2'},
+//     {id: 3, title: 'タイトル3', body: 'ボディー3'},
+// ]
+//
+// #after
+// state = [
+//     {id: 1, title: 'タイトル1', body: 'ボディー1'},
+//     {id: 2, title: 'タイトル2', body: 'ボディー2'},
+//     {id: 3, title: 'タイトル3', body: 'ボディー3'},
+//     {id: 4, title: 'タイトル4', body: 'ボディー4'},
+// ]
+
+const events = (state = [], action) => {
+  switch(action.type) {
+    case 'CREATE_EVENT':
+      const event = { title: action.title, body: action.body }
+      const length = state.length;
+      let id = length === 0 ? 1 : state[length - 1].id + 1;
+      return [...state, { id, ...event }];
+    case 'DELETE_EVENT':
+      // TODO
+      return state;
+    case 'DELETE_ALL_EVENT':
+      return [];
+    default:
+      return state;
+  }
+}
+
+export default events;
