cee14c3ecda77ce18a2f267a0772d054d8750364
yooda123 <yosukeee.o@gmail.com>
2020-12-20 19:09:52 +0900

    create operationLogs reducer

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,8 @@ console.log(AppContext);
 
 const App = () => {
   const initialState = {
-    events: []
+    events: [],
+    operationLogs: []
   }
   const [state, dispatch] = useReducer(reducer, initialState);
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import events from './events';
+import operationLogs from './operationLogs';
 
 export default combineReducers({
-  events
+  events,
+  operationLogs
 })
